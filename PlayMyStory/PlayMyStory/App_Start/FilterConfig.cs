f2598e0f3f26bf7d0eb873fc5cee7cf4a25b780d
yann surzur <yannsurzur@gmail.com>
2021-02-21 16:53:47 +0100

    Add the first aps.net project and some libs.

---
+++ PlayMyStory/PlayMyStory/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PlayMyStory
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
